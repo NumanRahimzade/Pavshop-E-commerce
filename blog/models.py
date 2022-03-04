e12025fb014dfbd7d51d1899fa3fbec1b32546cb
NumanRahimzade <numanrahimzade@gmail.com>
2022-03-04 23:25:45 +0400

    commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
+from core.models import AbstractModel
 from django.db import models
 
-# Create your models here.
+
+class Blog(AbstractModel):
+
+    title = models.CharField('Title', max_length=50)
+    description = models.TextField('Description')
+
+    def __str__(self):
+        return self.title
--- product/models.py
+++ product/models.py
@@ -1,3 +1,26 @@
+from core.models import AbstractModel
 from django.db import models
 
-# Create your models here.
+
+class Order(AbstractModel):
+
+    total = models.DecimalField('Total', decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return self.total
+
+
+class Basket(AbstractModel):
+    sub_total = models.DecimalField('Sub-Total', decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return self.sub_total
+
+
+class BasketItem(AbstractModel):
+    price = models.DecimalField('Price', decimal_places=2, max_digits=10)
+    sub_total = models.DecimalField('Sub-Total', decimal_places=2, max_digits=10)
+    count = models.IntegerField('Count')
+
+    def __str__(self):
+        return self.sub_total
