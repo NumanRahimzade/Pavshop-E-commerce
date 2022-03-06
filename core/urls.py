0d0ba086b4d49d94febea18d7aed24f39cfacc1c
NumanRahimzade <numanrahimzade@gmail.com>
2022-03-06 12:52:11 +0400

    task

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from core.views import home, about, contact
+
+
+urlpatterns = [
+    path('', home, name=''),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
+    
+
+]
