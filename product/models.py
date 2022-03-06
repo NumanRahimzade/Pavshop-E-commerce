df2bba0d7f4cfaa462b55cf54d4348b81be9bc8b
aysuqurbanli <aysuqurbanli00@gmail.com>
2022-03-06 11:23:48 +0400

    models

--- product/models.py
+++ product/models.py
@@ -1,3 +1,85 @@
 from django.db import models
 
-# Create your models here.
+
+class AbstractModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+    
+
+class Category(AbstractModel):
+    name=models.CharField('Name',max_length=70)
+    
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class PropertyName(AbstractModel):
+    name=models.CharField('Name',max_length=70)
+
+
+    class Meta:
+        verbose_name = 'Property Name'
+        verbose_name_plural = 'Propery Names'
+
+    def __str__(self):
+        return self.name
+
+
+class PropertyValues(AbstractModel):
+    value=models.CharField('Value',max_length=100)
+
+
+    class Meta:
+        verbose_name = 'Property Value'
+        verbose_name_plural = 'Property Values'
+
+    def __str__(self):
+        return self.value
+
+
+class ProductVersion(AbstractModel):
+    title=models.CharField('Title', max_length=50)
+    code=models.CharField('Code',max_length=50)
+    price=models.CharField('Price',max_length=40)
+    stock=models.IntegerField('Stock')
+
+    def __str__(self):
+        return self.title
+
+
+class Brand(AbstractModel):
+    name=models.CharField('Name',max_length=70)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImages(AbstractModel):
+    image = models.ImageField(upload_to='media/product_images/')
+    is_main=models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Product Image'
+        verbose_name_plural = 'Product Images'
+
+
+class Discount(AbstractModel):
+    title=models.CharField('Title', max_length=80)
+    percentage=models.CharField('Percentage', max_length=20)
+    value=models.IntegerField('Value')
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
