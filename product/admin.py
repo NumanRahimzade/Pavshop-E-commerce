8bfb42f4731f7b2d60fdaae6c2037c600b671ecd
aysuqurbanli <aysuqurbanli00@gmail.com>
2022-03-11 20:52:55 +0400

    project_new

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from product.models import Category,Product,PropertyName,PropertyValues,ProductVersion,Brand,ProductImages,Discount
+from product.models import Category,Product,PropertyName,PropertyValues,ProductVersion,Brand,ProductImages,Discount,WishList
 
 
-admin.site.register([Category,Product,PropertyName,PropertyValues,ProductVersion,Brand,ProductImages,Discount])
+admin.site.register([Category,Product,PropertyName,PropertyValues,ProductVersion,Brand,ProductImages,Discount,WishList])
