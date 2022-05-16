a3b40e87f5c984d608d27b401a7f0618bf4ccd73
NumanRahimzade <numanrahimzade@gmail.com>
2022-05-16 16:00:59 +0400

    ...

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -31,9 +31,6 @@ urlpatterns = [
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-admin.site.site_header = "PAVSHOP Admin"
-admin.site.site_title = "PAVSHOP Admin Portal"
-admin.site.index_title = "Welcome to PAVSHOP Portal"
 
 urlpatterns += i18n_patterns(
     path('', include('core.urls')),
