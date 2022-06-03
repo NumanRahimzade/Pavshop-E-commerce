e39ec3186711bb463b60b0ff43e902bd72726b6a
NumanRahimzade <numanrahimzade@gmail.com>
2022-06-03 15:40:31 +0400

    send message redis

--- core/publisher.py
+++ core/publisher.py
@@ -9,12 +9,6 @@ class Publish:
         self.data = data 
         self.event_type = event_type
         self._publish()
-        # data = {
-        #     "body": "Salam necesen?",
-        #     "subject": "Salam",
-        #     "recipients": ['idris.sabanli@gmail.com', 'yusif@gmail.com'],
-        #     "subtype": "html"
-        # }
         
 
     def _stringfy(self):
@@ -26,4 +20,3 @@ class Publish:
     def _publish(self):
         settings.REDIS_CLIENT.publish(self.channel_name, self._stringfy())
 
-        # stringfy() = '{"data": {"body": "Salam necesen?","subject": "Salam", "recipients": ["idris.sabanli@gmail.com", "yusif@gmail.com"], "subtype": "html"}, event_type="send_mail" }'
