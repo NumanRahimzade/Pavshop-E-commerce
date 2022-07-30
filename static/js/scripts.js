ad7a1c0f894afdadf698e9a808573833fb4a5007
NumanRahimzade <numanrahimzade@gmail.com>
2022-07-30 23:07:50 +0400

    http://127.0.0.1

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,7 +5,7 @@
        let postData = {
            "email": form.email.value
        }
-       let response = await fetch('http://localhost:8000/api/subscriptions/', {
+       let response = await fetch('http://127.0.0.1:8000/api/subscriptions/', {
             headers: {
                 'Content-Type': 'application/json',
                 },
